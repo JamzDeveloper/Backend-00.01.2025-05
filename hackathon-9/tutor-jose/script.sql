a778479abaa9970501617ffb5fbbecbaee4ee8c7
jamz <jamzdeveloper@gmail.com>
2025-07-26 10:00:08 -0500

    scripts

--- hackathon-9/tutor-jose/script.sql
+++ hackathon-9/tutor-jose/script.sql
@@ -22,3 +22,39 @@ where price>48
 
 select country, count(*) as "total" from customers
 group by country
+
+
+--Obtener todos los pedidos realizados en el año 1997.
+
+select * from orders
+where extract(year from orderdate) =1997
+
+
+--select distinct extract(year from orderdate) from orders
+
+
+--Mostrar todos los productos en la categoría "Beverages".
+
+select * from categories;
+
+
+select * from products
+inner join categories
+on products.categoryid= categories.categoryid
+where categories.categoryname='Beverages';
+
+
+--Mostrar los 10 productos más caros.
+
+select  * from products
+order by price desc
+limit 10;
+
+
+
+--Listar el número de pedidos por cliente.
+
+
+select customerid,count(*) from  orders
+
+group by customerid
